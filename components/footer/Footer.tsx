577bd46f438661da15b94970b198985cff30c047
XFaonAE <xfaonae@protonmail.com>
2021-12-20 12:23:14 -0500

    Changes

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import style from "./Footer.module.scss";
+
+export default function Footer() {
+    return (
+        <div className={style.root}>
+            <div className={style.inner}>
+                <div className={style.header}>
+
+                </div>
+
+                
+            </div>
+        </div>
+    );
+}
