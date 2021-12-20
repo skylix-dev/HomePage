3f71690853715216f9347d0595377d7d3f94c2ac
XFaon <xfaonae@protonmail.com>
2021-12-20 07:01:56 -0500

    Added homepage banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
 import style from "../styles/Index.module.scss";
 
 export default function Index() {
-	return <span>Home Page</span>;
+	return (
+		<div className={style.root}>
+			<div className={style.banner}>
+				<h1>Changing Code Forever!</h1>
+				<span>ILLUX | The open source development team!</span>
+
+				<div className={style.bannerButtons}>
+					<button className={style.bannerButtonsPrimary}>Get Started</button>
+					<button>Documentation</button>
+				</div>
+			</div>
+		</div>
+	);
 }
