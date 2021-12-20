4350e03d3d64f086e772843a2c1c61e252729b36
XFaon <xfaonae@protonmail.com>
2021-12-19 21:37:49 -0500

    Added page scrollbar and fixed content area system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
+import Head from "next/head";
 import NavBar from "../components/navBar/NavBar";
 import "../styles/Globals.scss";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<div className="root">
+			<Head>
+				<title>ILLUX</title>
+			</Head>
 			<NavBar />
 
 			<div className="body">
-				<Component {...pageProps} />
-			</div>
+				<div className="main">
+					<Component {...pageProps} />
+				</div>
 
-			<footer>ILLUX - CopyRight 2021 - 2021</footer>
+				<footer>ILLUX - CopyRight 2021 - 2021</footer>
+			</div>
 		</div>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import style from "../styles/Index.module.scss";
+
 export default function Index() {
 	return <span>Home Page</span>;
 }
