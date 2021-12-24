b1c72e295d5312dabfc9a725f1badf6203a1e85d
EmeraldMike <36397097+EmeraldMike@users.noreply.github.com>
2021-12-23 20:12:21 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<div className="root">
 			<Head>
-				<title>ILLUX</title>
+				<title>SkyLix</title>
 				<link rel="icon" type="image/svg+xml" href="https://raw.githubusercontent.com/IlluxDev/Illux/main/LogoDynamic.svg" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
